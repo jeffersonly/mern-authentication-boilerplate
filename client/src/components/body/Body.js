d95260e40fe95c86e906c5e19e9a3af6b2d886c9
Jefferson Ly <32202953+jeffersonly@users.noreply.github.com>
2021-04-05 21:58:50 -0700

    Handle logout and access user info func in frontend

--- client/src/components/body/Body.js
+++ client/src/components/body/Body.js
@@ -3,13 +3,18 @@ import { Switch, Route } from "react-router-dom";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
 import AccountActivation from "./auth/AccountActivation";
+import NotFound from "../utils/not_found/NotFound";
+import { useSelector } from "react-redux";
 
 function Body() {
+    const auth = useSelector(state => state.auth);
+    const { isLogged } = auth;
+
     return (
         <section>
             <Switch>
-                <Route path="/login" component={Login} exact />
-                <Route path="/register" component={Register} exact />
+                <Route path="/login" component={isLogged ? NotFound : Login} exact />
+                <Route path="/register" component={isLogged ? NotFound : Register} exact />
                 <Route path="/user/activate/:activation_token" component={AccountActivation} exact />
             </Switch>
         </section>
--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,7 +1,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import axios from "axios";
 
 function Header() {
+    const auth = useSelector(state => state.auth);
+    
+    const { user, isLogged } = auth;
+
+    const handleLogout = async () => {
+        try {
+            await axios.get('/user/logout');
+            localStorage.removeItem('firstLogin');
+            window.localtion.href = "/";
+        } catch(err) {
+            window.location.href = "/";
+        }
+    };
+
+    const userLink = () => {
+        return (
+            <li className="drop-nav">
+                <Link to="#">
+                    <img src={user.avatar} className="avatar" /> {user.name} <i className="fas fa-angle-down"></i>
+                </Link>
+
+                <ul className="dropdown">
+                    <li>
+                        <Link to="/profile">Profile</Link>
+                    </li>
+
+                    <li>
+                        <Link to="/" onClick={handleLogout}>Logout</Link>
+                    </li>
+                </ul>
+            </li>
+        );
+    };
+
+    const transformStyle = {
+        transform: isLogged ? "translateY(-5px)" : 0
+    };
+
     return (
         <header>
             <div className="logo">
@@ -10,7 +50,7 @@ function Header() {
                 </h1>
             </div>
 
-            <ul>
+            <ul style={transformStyle}>
                 <li>
                     <Link to="/">
                         <i className="fas fa-home"></i>
@@ -18,12 +58,19 @@ function Header() {
                     </Link>
                 </li>
 
-                <li>
-                    <Link to="/login">
-                        <i className="fas fa-user"></i>
-                        Sign In
-                    </Link>
-                </li>
+                {
+                    isLogged ? userLink() :
+                    (
+                        <li>
+                            <Link to="/login">
+                                <i className="fas fa-user"></i>
+                                Sign In
+                            </Link>
+                        </li>
+                    )
+                }
+
+                
             </ul>
         </header>
     );
